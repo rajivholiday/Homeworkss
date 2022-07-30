2fd1f4204bb4f2edd75fae2ccfd82f79341afb35
Raji <rajbaymas@gmail.com>
2022-07-30 17:37:58 +0400

    printf method print etmir

--- src/main/java/Lesson8/InchToMeter.java
+++ src/main/java/Lesson8/InchToMeter.java
@@ -1,5 +1,4 @@
 package Lesson8;
-import java.text.DecimalFormat;
 
 import java.text.DecimalFormat;
 import java.util.Scanner;
@@ -11,9 +10,8 @@ public class InchToMeter {
         System.out.println("Input a value for inch:");
         double inch = scanner.nextDouble();
         double meter = inch * 0.254;
-        DecimalFormat df = new DecimalFormat("0.#");
-
-        System.out.printf("%f inch is %f meters", (df.format(inch)), (df.format(meter)));
+        DecimalFormat df = new DecimalFormat("0.0#");
+        System.out.printf("%f inch is %d meters", df.format(inch), df.format(meter));
 
     }
 }
