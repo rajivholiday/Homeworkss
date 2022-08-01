5b670b85c18b9c8b5d1c59a03a219bdf3dfe4d28
Raji <rajbaymas@gmail.com>
2022-08-01 20:51:27 +0400

    initial commit

---
+++ src/main/java/Lesson8/Main.java
@@ -0,0 +1,9 @@
+package Lesson8;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] habits = {"Singing", "Exercising", "Movie", "Hockey"};
+        var person = new PersonPrinting("Mario", "Gomes", 35, 1987, habits);
+        System.out.println(person.toString());
+    }
+}
