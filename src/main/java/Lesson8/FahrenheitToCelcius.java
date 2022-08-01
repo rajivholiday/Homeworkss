a418a352ef8ffc85ef360c13938f6238115ca355
Raji <rajbaymas@gmail.com>
2022-08-01 14:34:11 +0400

    fahrenheit de vergulden sonra bir 0, selside ise iki sifir qalir?

--- src/main/java/Lesson8/FahrenheitToCelcius.java
+++ src/main/java/Lesson8/FahrenheitToCelcius.java
@@ -1,14 +1,16 @@
 package Lesson8;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class FahrenheitToCelcius {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Input a degree fahrenheit:");
+        DecimalFormat df = new DecimalFormat("0.0#");
         double f = scanner.nextDouble();
         double c = (f - 32) * 0.5556;
-        System.out.printf("%f degree Fahrenheit is equal to %f in Celcius", f, c);
+        System.out.println(df.format(f) + " degree Fahrenheit is equal to " + df.format(c) + " in Celcius");
     }
 }
 
--- src/main/java/Lesson8/MinutToYear.java
+++ src/main/java/Lesson8/MinutToYear.java
@@ -7,9 +7,9 @@ public class MinutToYear {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number of minutes");
         int minut = scanner.nextInt();
-        double year = minut/525600;
-        int days = (int) (minut/60/24)%365;
-        System.out.printf("%d is %f years and %d days", minut, year, days);
+        int year = (int) (minut / 525600);
+        int days = (int) (minut / 60 / 24) % 365;
+        System.out.printf("%f is %f years and %d days", minut, year, days);
 
 
     }
