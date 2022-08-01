fce6b76bbf0ecee8104b21d2da51ccd8bd67cc09
Raji <rajbaymas@gmail.com>
2022-08-01 19:15:05 +0400

    initial commit

--- src/main/java/Lesson8/MinutToYear.java
+++ src/main/java/Lesson8/MinutToYear.java
@@ -6,11 +6,11 @@ public class MinutToYear {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number of minutes");
-        int minut = scanner.nextInt();
-        int year = (int) (minut / 525600);
+        double minut = scanner.nextDouble();
+        double minutesInYear = 60 * 24 * 365;
+        int year = (int) (minut / minutesInYear);
         int days = (int) (minut / 60 / 24) % 365;
-        System.out.printf("%f is %f years and %d days", minut, year, days);
-
+        System.out.println((int) minut + " minutes is approximately " + year + " years and " + days + " days");
 
     }
 }
