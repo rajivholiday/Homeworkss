dd5ab16aed4fba0a96e482e849fa3655b6129a69
Raji <rajbaymas@gmail.com>
2022-08-03 20:23:50 +0400

    initial commit

---
+++ src/main/java/lesson9/IndexOfArrayElement.java
@@ -0,0 +1,21 @@
+package lesson9;
+
+public class IndexOfArrayElement {
+    public static int find(int[] a, int target) {
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        var index = new IndexOfArrayElement();
+        int[] a = {20, 15, 3, 32};
+        System.out.println(find(a, 32));
+        System.out.println(find(a, 99));
+
+
+    }
+}
