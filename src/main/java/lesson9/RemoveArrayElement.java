65a1d0daa88e83f8adb8c0c7b0135f3581ffadfe
Raji <rajbaymas@gmail.com>
2022-08-06 11:41:19 +0400

    initial commit

---
+++ src/main/java/lesson9/RemoveArrayElement.java
@@ -0,0 +1,22 @@
+package lesson9;
+
+import java.util.Arrays;
+
+public class RemoveArrayElement {
+    public static String removeElement(int[] arr, int target) {
+        int[] new_arr = new int[arr.length - 1];
+        for (int i = 0, j = 0; i < arr.length; i++) {
+            if (arr[i] != target) {
+                new_arr[j++] = arr[i];
+            }
+
+        }
+        return Arrays.toString(new_arr);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int target = 5;
+        System.out.println(removeElement(arr, target));
+    }
+}
