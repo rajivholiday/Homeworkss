257e27830ff71b562352ba73fcb50e487c2fded3
Raji <rajbaymas@gmail.com>
2022-08-14 14:57:54 +0400

    initial commit

--- src/main/java/HappyFamily2/Main.java
+++ src/main/java/HappyFamily2/Main.java
@@ -32,9 +32,11 @@ public class Main {
 
         Human mother_b = new Human("Maria", "Balotelli", 1990, 100, schedule_M);
         Human father_b = new Human("Francesko", "Balotelli", 1988, 105, schedule_F);
-        Human[] children_b = new Human[2];
+        Human[] children_b = new Human[3];
         children_b[0] = new Human("Max", "Balotelli", 2000);
         children_b[1] = new Human("Martina", "Balotelli", 2002);
+
+
 
 
         Family gomes = new Family(mother, father, children);
