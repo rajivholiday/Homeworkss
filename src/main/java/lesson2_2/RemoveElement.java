02e51a79c281547eaf4c76d5ce872c4314e80284
Raji <rajbaymas@gmail.com>
2022-08-19 19:50:22 +0400

    Object reference was removed, static modifier added to method

--- src/main/java/lesson2_2/RemoveElement.java
+++ src/main/java/lesson2_2/RemoveElement.java
@@ -4,15 +4,15 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class RemoveElement {
-    public ArrayList<Integer> removeThirdEl(ArrayList<Integer> numbers) {
+    public static ArrayList<Integer> removeThirdEl(ArrayList<Integer> numbers) {
         numbers.remove(2);
         return numbers;
     }
 
     public static void main(String[] args) {
-        RemoveElement new_arr = new RemoveElement();
         ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(20, 2, 8, 50));
-        System.out.println(new_arr.removeThirdEl(numbers));
+        System.out.println(removeThirdEl(numbers));
+
 
     }
 }
