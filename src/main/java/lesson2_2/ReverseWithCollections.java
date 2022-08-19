8f9a9ed6a78536f7de7feda6eafb50b556e7bd14
Raji <rajbaymas@gmail.com>
2022-08-19 19:34:48 +0400

    Object reference was removed, static modifier added to method

--- src/main/java/lesson2_2/ReverseWithCollections.java
+++ src/main/java/lesson2_2/ReverseWithCollections.java
@@ -5,20 +5,18 @@ import java.util.Arrays;
 import java.util.Collections;
 
 public class ReverseWithCollections {
-    public ArrayList<Integer> rev(ArrayList<Integer> numbers) {
+    public static ArrayList<Integer> rev(ArrayList<Integer> numbers) {
         ArrayList<Integer> reverse_numbers = new ArrayList<>(numbers);
         Collections.reverse(reverse_numbers);
         return reverse_numbers;
     }
 
     public static void main(String[] args) {
-        ReverseWithCollections obj = new ReverseWithCollections();
         ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
-        ArrayList<Integer> reverse_numbers = obj.rev(numbers);
+        ArrayList<Integer> reverse_numbers = rev(numbers);
 
-        System.out.print("Elements before reversing " + numbers + "\n");
-        System.out.print("Elements after reversing " + reverse_numbers);
-
+        System.out.printf("Elements of the array: %s \n", numbers.toString());
+        System.out.printf("Elements of the reverse array: %s \n", reverse_numbers.toString());
 
     }
 }
