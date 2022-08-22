c33b678336dde36fd50d4b3dca03f5b0855e4e84
Raji <rajbaymas@gmail.com>
2022-08-22 23:15:19 +0400

    Before each də yaratdığım field lər digər method larda görünmür Ona görə hər dəfə manual yazdım

--- src/test/java/happyfamily3/FamilyTest.java
+++ src/test/java/happyfamily3/FamilyTest.java
@@ -9,20 +9,61 @@ class FamilyTest {
     Family new_Family;
 
     @BeforeEach
-    public void setUp(){
+    public void setUp() {
+        Human mother = new Human("Andrea", "Rossi", 1982);
+        Human father = new Human("Martin", "Rossi", 1980);
+        Human[] children = new Human[0];
+        Family rossi = new Family(mother, father, children);
 
     }
 
+
     @Test
     void addChild() {
+        Human mother = new Human("Andrea", "Rossi", 1980);
+        Human father = new Human("Martin", "Rossi", 1978);
+        Human[] children = new Human[0];
+        Family rossi = new Family(mother, father, children);
+        Human roberto = new Human("Roberto", "Rossi", 2005);
+        rossi.addChild(roberto);
+
+        String result = "Family{ \n" +
+                "mother = Human{name = Andrea, surname = Rossi, year = 1980, iq = 0, schedule = null},\n" +
+                "father = Human{name = Martin, surname = Rossi, year = 1978, iq = 0, schedule = null},\n" +
+                "children = [Human{name = Roberto, surname = Rossi, year = 2005, iq = 0, schedule = null}],\n" +
+                "pet = null";
+        assertEquals(result, rossi.toString());
+
+
     }
 
     @Test
     void deleteChild() {
+        Human mother = new Human("Andrea", "Rossi", 1980);
+        Human father = new Human("Martin", "Rossi", 1978);
+        Human[] children = new Human[0];
+        Family rossi = new Family(mother, father, children);
+        Human roberto = new Human("Roberto", "Rossi", 2005);
+        rossi.addChild(roberto);
+        rossi.deleteChild(0);
+        String result = "Family{ \n" +
+                "mother = Human{name = Andrea, surname = Rossi, year = 1980, iq = 0, schedule = null},\n" +
+                "father = Human{name = Martin, surname = Rossi, year = 1978, iq = 0, schedule = null},\n"
+                + "children = [],\n" +
+                "pet = null";
+        assertEquals(result, rossi.toString());
     }
 
     @Test
     void countFamily() {
+        Human mother = new Human("Andrea", "Rossi", 1980);
+        Human father = new Human("Martin", "Rossi", 1978);
+        Human[] children = new Human[0];
+        Family rossi = new Family(mother, father, children);
+        Human roberto = new Human("Roberto", "Rossi", 2005);
+        rossi.addChild(roberto);
+        int i = 2 + children.length;
+        assertEquals(3, rossi.countFamily());
 
 
     }
