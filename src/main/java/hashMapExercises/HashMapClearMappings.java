d330a3ef665e3d60b8d6b69ccc5ed70117d65241
Raji <rajbaymas@gmail.com>
2022-08-29 17:28:21 +0400

    initial commit

---
+++ src/main/java/hashMapExercises/HashMapClearMappings.java
@@ -0,0 +1,12 @@
+package hashMapExercises;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class HashMapClearMappings {
+    public static HashMap<String, String> clear(HashMap<String, String> cities) {
+        cities.clear();
+        return cities;
+    }
+
+}
