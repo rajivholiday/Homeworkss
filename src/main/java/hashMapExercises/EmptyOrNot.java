2a8c6c06a5d26e6521ee3d05f6ea6dbaa1923662
Raji <rajbaymas@gmail.com>
2022-08-29 17:37:26 +0400

    initial commit

---
+++ src/main/java/hashMapExercises/EmptyOrNot.java
@@ -0,0 +1,9 @@
+package hashMapExercises;
+
+import java.util.HashMap;
+
+public class EmptyOrNot {
+    public static boolean emptyOrNot(HashMap<String, String> names) {
+        return names.isEmpty();
+    }
+}
