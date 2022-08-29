455c514ecb0d264a363303500dbd60e2e6f30d06
Raji <rajbaymas@gmail.com>
2022-08-29 18:31:35 +0400

    initial commit

---
+++ src/main/java/hashMapExercises/ValueSetView.java
@@ -0,0 +1,9 @@
+package hashMapExercises;
+
+import java.util.HashMap;
+
+public class ValueSetView {
+    public static void printValues(HashMap<String, String> names) {
+        System.out.println(names.values());
+    }
+}
