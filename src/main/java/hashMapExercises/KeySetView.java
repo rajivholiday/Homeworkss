666f21acc8287c8272c79c1624e0a681a971090a
Raji <rajbaymas@gmail.com>
2022-08-29 18:29:09 +0400

    initial commit

---
+++ src/main/java/hashMapExercises/KeySetView.java
@@ -0,0 +1,12 @@
+package hashMapExercises;
+
+import java.util.HashMap;
+
+
+public class KeySetView {
+    public static void printKeys(HashMap<String, Integer> names) {
+        System.out.println(names.keySet());
+
+    }
+
+}
