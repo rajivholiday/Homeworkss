5f3293689f7804b65c9581ce4d715fdd7a80fba1
Raji <rajbaymas@gmail.com>
2022-08-29 17:17:19 +0400

    initial commit

---
+++ src/main/java/hashMapExercises/HashMapSize.java
@@ -0,0 +1,10 @@
+package hashMapExercises;
+
+import java.util.HashMap;
+
+public class HashMapSize {
+    public static int size(HashMap<String, String> cities) {
+        return cities.size();
+    }
+
+}
