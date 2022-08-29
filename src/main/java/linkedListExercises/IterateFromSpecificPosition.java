6599ea0cdb1f097537d99e79839ce8d5f284de45
Raji <rajbaymas@gmail.com>
2022-08-29 20:40:20 +0400

    initial commit

---
+++ src/main/java/linkedListExercises/IterateFromSpecificPosition.java
@@ -0,0 +1,14 @@
+package linkedListExercises;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class IterateFromSpecificPosition {
+    public static void iterator(LinkedList<Integer> nums) {
+        Iterator it = nums.listIterator(2);
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+    }
+
+}
