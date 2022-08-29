af1fc14cd413984d17e4a2620f659846e495dd75
Raji <rajbaymas@gmail.com>
2022-08-29 18:22:16 +0400

    initial commit

---
+++ src/main/java/hashMapExercises/SetViewOfMappings.java
@@ -0,0 +1,11 @@
+package hashMapExercises;
+
+import java.util.HashMap;
+import java.util.Set;
+
+public class SetViewOfMappings {
+    public static void setView(HashMap<Integer, String> names) {
+        System.out.println(names.entrySet());
+
+    }
+}
