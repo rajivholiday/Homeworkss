25d67b5baa25828ced94cbf0773ee6be96495a5b
Raji <rajbaymas@gmail.com>
2022-08-29 22:08:58 +0400

    initial commit

---
+++ src/main/java/linkedListExercises/AddLast.java
@@ -0,0 +1,10 @@
+package linkedListExercises;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class AddLast {
+    public static void addLastElement(LinkedList<String> names, String name) {
+        names.addLast(name);
+    }
+}
