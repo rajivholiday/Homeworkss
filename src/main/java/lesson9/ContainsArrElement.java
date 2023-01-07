1fd6422c952903448a4ce741ab7c2ea778df007e
Raji <rajbaymas@gmail.com>
2023-01-07 10:50:44 +0400

    initial commit

--- src/main/java/lesson9/ContainsArrElement.java
+++ src/main/java/lesson9/ContainsArrElement.java
@@ -8,14 +8,11 @@ public class ContainsArrElement {
         int[] arr = {1, 2, 3, 4, 5};
         System.out.println(containsElement(arr, 4));
         System.out.println(containsElement(arr, 10));
-
-
-
     }
 
     public static boolean containsElement(int[] arr, int target) {
-        for (int i = 0; i < arr.length; i++) {
-            if (target == arr[i]) {
+        for (int i : arr) {
+            if (target == i) {
                 return true;
             }
         }
