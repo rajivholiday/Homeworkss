63e980fc3a09b2f0605de9352ac6ef7529f3aa9f
Raji <rajbaymas@gmail.com>
2023-01-07 10:41:33 +0400

    initial commit

--- src/main/java/linkedListExercises/LinkedListIteration.java
+++ src/main/java/linkedListExercises/LinkedListIteration.java
@@ -1,13 +1,15 @@
 package linkedListExercises;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 
 public class LinkedListIteration {
     public static void iterator(LinkedList<Integer> nums) {
-        nums.forEach(n -> System.out.println(n));
         nums.forEach(System.out::println);
-        for (Integer i : nums) {
-            System.out.println(i);
-        }
+        for (Integer i : nums) System.out.println(i);
+
+        Iterator<Integer> it = nums.iterator();
+        while (it.hasNext())
+            System.out.println(it.next());
     }
 }
