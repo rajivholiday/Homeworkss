4cc0e58f67190c62421b1aac5f7c4ea07e5de9d0
Raji <rajbaymas@gmail.com>
2023-01-07 10:28:59 +0400

    initial commit

--- src/main/java/linkedListExercises/IterateReverseOrder.java
+++ src/main/java/linkedListExercises/IterateReverseOrder.java
@@ -1,18 +1,20 @@
 package linkedListExercises;
 
+import java.time.LocalTime;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedList;
 
 public class IterateReverseOrder {
 
     public static void iterator(LinkedList<Integer> nums) {
-        for (int i = nums.size() - 1; i >= 0; i--) {
+        for (int i = nums.size() - 1; i >= 0; i--) {  // reversing with for loop
             System.out.println(nums.get(i));
         }
 
-        Collections.reverse(nums);
-        nums.forEach(n -> System.out.println(n));
+        Collections.reverse(nums);                // reversing with collection
+        nums.forEach(System.out::println);
 
-        nums.stream().sorted(Collections.reverseOrder()).forEach(n -> System.out.println(n));
+        nums.stream().sorted(Collections.reverseOrder()).forEach(System.out::println);
     }
 }
