351927dde086fa5149d05e027462264c40d68b84
Raji <rajbaymas@gmail.com>
2023-01-07 10:55:13 +0400

    initial commit

--- src/main/java/lesson9/MakeArrayCopy.java
+++ src/main/java/lesson9/MakeArrayCopy.java
@@ -7,15 +7,12 @@ public class MakeArrayCopy {
         int[] new_arr = new int[arr.length];
         for (int i = 0; i < arr.length; i++) {
             new_arr[i] = arr[i];
-
         }
         return Arrays.toString(new_arr);
     }
-
     public static void main(String[] args) {
         int[] arr = {10, 4, 6, 8, 23, 5, 100};
         System.out.println("New array: " + ArrayCopy(arr));
-
 
     }
 }
