88223c93538ac4271609f4fa1fa9977afded615d
Raji <rajbaymas@gmail.com>
2023-01-07 11:03:36 +0400

    initial commit

--- src/main/java/lesson2_2/MergeArrays.java
+++ src/main/java/lesson2_2/MergeArrays.java
@@ -7,14 +7,11 @@ public class MergeArrays {
     public ArrayList<String> mergeArrays(ArrayList<String> cities1,ArrayList<String> cities2) {
        cities2.addAll(cities1);
        return  cities2;
-
     }
 
     public static void main(String[] args) {
         ArrayList<String> cities1 = new ArrayList<>(Arrays.asList("Tokyo", "Islamabad", "Oslo", "Roma"));
         ArrayList<String> cities2 = new ArrayList<>(Arrays.asList("Baku", "Minsk", "Madrid"));
-
-
 
     }
 }
