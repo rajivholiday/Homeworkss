079afc436d5145abe75074d5291c8a1d109235f0
Raji <rajbaymas@gmail.com>
2023-01-07 11:07:45 +0400

    initial commit

--- src/main/java/Palindrome.java
+++ src/main/java/Palindrome.java
@@ -1,9 +1,6 @@
 public class Palindrome {
     public static boolean checkIfPalindrome(String s1) {
         String s2 = new StringBuffer(s1).reverse().toString();
-        if (s1.equals(s2)) {
-            return true;
-        }
-        return false;
+        return s1.equals(s2);
     }
 }
